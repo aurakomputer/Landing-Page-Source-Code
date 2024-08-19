570fcc3238102070819c655aea503bb1764e1c6d
Mohamad Supangat <moha.supangat@gmail.com>
2024-08-19 22:30:10 +0700

    detail product preview using glightbox

--- themes/aurakomputer/assets/js/main.js
+++ themes/aurakomputer/assets/js/main.js
@@ -96,14 +96,6 @@
     scrollTo(document.documentElement);
   };
 
-  // image preview picker
-  // --------------------------------------------------
-  for (const el of document.getElementsByClassName("images-preview")) {
-    el.onclick = (event) => {
-      document.getElementById("image-preview").src = el.dataset.img;
-    };
-  }
-
   const selectPaket = document.getElementsByClassName("selectPaket");
 
   // form checkout button
